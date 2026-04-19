d74334ce7818637b4459ee55ea58d174b4b6535f
aayush <aayushsuthar2004@gmail.com>
2026-04-19 14:36:58 +0530

    feat: bagel token

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract MerkleAirdrop {
+    // some list of address
+    // Allow someone in the list to claim token
+    address[] claimers;
+
+    // merkle proofs
+}
