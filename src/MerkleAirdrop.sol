4f958767c3bbf03fe8ce08e783b900042a5ad1f1
aayush <aayushsuthar2004@gmail.com>
2026-04-19 15:33:10 +0530

    feat: build basic markle tree and some test

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -9,6 +9,7 @@ contract MerkleAirdrop {
     using SafeERC20 for IERC20;
 
     error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
     event Claim(address account, uint256 amount);
 
@@ -17,6 +18,7 @@ contract MerkleAirdrop {
     address[] claimers;
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) s_hasClaimed;
 
     // merkle proofs
     constructor(bytes32 merkleRoot, IERC20 airdropToken) {
@@ -24,15 +26,31 @@ contract MerkleAirdrop {
         i_airdropToken = airdropToken;
     }
 
+    // follow CEI
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
         // calculate using the account and the amount, the hash -> leaf node
         // to avoid collision, we hash them twice
+        
+        if(s_hasClaimed[account]){
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
         if(!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
             revert MerkleAirdrop__InvalidProof();
         }
+        s_hasClaimed[account] = true;
         emit Claim(account, amount);
         // if we not able to send the tokens by any reason, it will handle it
         i_airdropToken.safeTransfer(account, amount);
     }
+
+    function getMerkleRoot() external view returns(bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
+
 }
